28185e44768c29b56f031924cad2ee52b9f67231
José Cavalcante Reis Neto <netocavalcante@gmail.com>
2018-10-10 14:46:05 -0300

    preparando para enviar para o digital ocean

--- saude/urls.py
+++ saude/urls.py
@@ -3,11 +3,14 @@ from . import views
 from rest_framework import routers
 from .viewsets import SamuView
 from .viewsets import HospitalView
-
+from .viewsets import UnidadeSaudeView
+from .viewsets import ServicoVeiw
 
 router = routers.DefaultRouter()
 router.register(r'saude', SamuView)
 router.register(r'hospitais', HospitalView)
+router.register(r'unidadesaude', UnidadeSaudeView)
+router.register(r'servico', ServicoVeiw)
 
 urlpatterns = [
     path('' , include(router.urls)),
